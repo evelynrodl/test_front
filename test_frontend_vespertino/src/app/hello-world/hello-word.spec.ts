fed9bbc05a09f70103e242567eaef849540245df
evelynrodl <evelyn.r.silva@ba.estudante.senai.br>
2026-09-10 16:05:26 -0300

    Terceiro commit'

---
+++ test_frontend_vespertino/src/app/hello-world/hello-word.spec.ts
@@ -0,0 +1,33 @@
+import { DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { describe, it, expect, beforeEach } from "vitest";
+import { HelloWorld } from './hello-world';
+
+describe('HelloWord', () => {
+    let fixture: ComponentFixture<HelloWorld>;
+    let de: DebugElement;
+    let el: HTMLElement;
+    let component: HelloWorld;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [HelloWorld]
+        }).compileComponents();
+        fixture = TestBed.createComponent(HelloWorld);
+        de = fixture.debugElement;
+        el = de.nativeElement;
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    })
+
+it('should create the component', () => {
+    expect(fixture.componentInstance).toBeDefined();
+})
+
+it('should display the message', () => {
+    const h1 = el.querySelector("h1");
+    expect(h1).toBeDefined
+    expect(h1?.textContent).toEqual(component.message);
+})
+
+})
